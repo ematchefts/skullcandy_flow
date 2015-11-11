6d12262ebc0ecb85e1d83aeb31618420899dc147
Jerome Francis Vogt <Jerome Francis Vogt>
2015-11-10 21:15:46 -0500

    Erased addtional system.out

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -18,7 +18,6 @@ public class JavaApplication2 {
    public static void main(String[] args) { 
        //outputs greeting to the world in the console
       System.out.println("Hello, World");
-      System.out.println();
    }
     
 }
